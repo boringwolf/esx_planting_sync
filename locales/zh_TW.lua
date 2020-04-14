bf39571605f72114a514749568f5d1174e470b06
boringwolf <ceo@bnsw.me>
2020-04-14 20:33:14 +0800

    add locale file

---
+++ locales/zh_TW.lua
@@ -0,0 +1,7 @@
+Locales['zh_TW'] = {
+	['weed_pickupprompt'] = '按 ~INPUT_CONTEXT~ 收穫',
+	['weed_inventoryfull'] = '背包空間不足',
+	['threw_pickup_prompt'] = '使用~y~E~s~收穫。',
+	['wrong_place'] = '這裡不適合種植',
+	['cancel'] = '取消'
+}
